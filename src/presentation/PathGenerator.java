4513baea0886cf1a2629997b7e093517dc296fac
Gonzalo Solera <gonzalo1796@gmail.com>
2016-05-24 17:40:15 +0200

    new cool version of pathgenerator

--- src/presentation/PathGenerator.java
+++ src/presentation/PathGenerator.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 public class PathGenerator extends JPanel implements ActionListener{
@@ -15,11 +14,19 @@ public class PathGenerator extends JPanel implements ActionListener{
     private MainView mainView;
     protected ArrayList<Integer> actualRS = new ArrayList<Integer>();
     private NodeType from = null;
-    private RoundButton[] buttons = new RoundButton[5];
+
+    private JPanel panel;
+
+    private RoundButton label;
+    private RoundButton term;
+    private RoundButton paper;
+    private RoundButton author;
+    private RoundButton conference;
     private JButton reset;
 
-    private static float MAX_ALPHA = 0.8f;
-    private static float MIN_ALPHA = 0.2f;
+    private RoundButton[] buttons;
+
+
     private static String LABEL = "Lab";
     private static String AUTHOR = "Aut";
     private static String PAPER = "Pap";
@@ -29,33 +36,7 @@ public class PathGenerator extends JPanel implements ActionListener{
     protected PathGenerator(PresentationController presentationController, MainView mainView) {
         this.presentationController = presentationController;
         this.mainView = mainView;
-        reset = new JButton("Reset");
-        reset.setVisible(false);
-        reset.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        reset.setVisible(false);
-                        mainView.setAddNodeButton(false);
-                        from = null;
-                        setEnabledButtons(true);
-                        actualRS = new ArrayList<Integer>();
-                    }
-                }
-        );
-
-        setLayout(new GridLayout(3, 3, 10, 10));
-        JPanel resetPanel = new JPanel();
-        resetPanel.add(reset, BorderLayout.PAGE_START);
-        add(resetPanel);
-        add(createButton(0, LABEL));
-        add(new JPanel());
-        add(createButton(1, AUTHOR));
-        add(createButton(2, PAPER));
-        add(createButton(3, CONFERENCE));
-        add(new JPanel());
-        add(createButton(4, TERM));
-        add(new JPanel());
+        add(panel);
     }
 
     @Override
@@ -84,53 +65,46 @@ public class PathGenerator extends JPanel implements ActionListener{
         setEnabledButtons(false);
         for(int i = 0; i < info.availableNodeTypes.size(); ++i) {
             int id = getButtonID(info.availableNodeTypes.get(i));
-            buttons[id].setOpacity(MAX_ALPHA);
-            //buttons[id].setBackground(getButtonColor(id));
             buttons[id].setEnabled(true);
         }
         from = to;
     }
 
-    private RoundButton createButton(int id, String name) {
-        RoundButton button = new RoundButton(name);
-        button.addActionListener(this);
-        button.setActionCommand(name);
-        button.setPreferredSize(new Dimension(100, 100));
-        button.setBackground(getButtonColor(id));
-        button.setOpacity(MAX_ALPHA);
-        button.setFont(new Font("Arial", Font.PLAIN, 40));
-        buttons[id] = button;
-        return button;
+    private void createUIComponents() {
+        label = new RoundButton(LABEL, Color.YELLOW);
+        author = new RoundButton(AUTHOR, Color.YELLOW);
+        paper = new RoundButton(PAPER, Color.YELLOW);
+        conference = new RoundButton(CONFERENCE, Color.YELLOW);
+        term = new RoundButton(TERM, Color.YELLOW);
+        buttons = new RoundButton[]{label, author, paper, conference, term};
+        for(int id = 0; id < buttons.length; ++id) {
+            buttons[id].addActionListener(this);
+            buttons[id].setPreferredSize(new Dimension(100, 100));
+            buttons[id].setFont(new Font("Arial", Font.PLAIN, 20));
+        }
+
+        reset = new JButton("Reset");
+        reset.setVisible(false);
+        reset.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        reset.setVisible(false);
+                        mainView.setAddNodeButton(false);
+                        from = null;
+                        setEnabledButtons(true);
+                        actualRS = new ArrayList<Integer>();
+                    }
+                }
+        );
     }
 
     protected void setEnabledButtons(boolean enable) {
         actualRS = new ArrayList<Integer>();
         for(int id = 0; id < buttons.length; ++id) {
-            if(!enable) {
-                buttons[id].setOpacity(MIN_ALPHA);
-                //buttons[id].setBackground(Color.GRAY);
-            } else {
-                buttons[id].setOpacity(MAX_ALPHA);
-                //buttons[id].setBackground(getButtonColor(id));
-            }
             buttons[id].setEnabled(enable);
         }
         from = null;
-    }
-
-
-    private Color getButtonColor(int id) {
-        if(id == 0) {
-            return Color.YELLOW;
-        } else if(id == 1) {
-            return Color.CYAN;
-        } else if(id == 2) {
-            return Color.RED;
-        } else if(id == 3) {
-            return Color.GREEN;
-        } else {
-            return Color.ORANGE;
-        }
     }
 
     private int getButtonID(NodeType type) {
@@ -158,57 +132,6 @@ public class PathGenerator extends JPanel implements ActionListener{
             return NodeType.CONF;
         } else {
             return NodeType.TERM;
-        }
-    }
-
-    private class RoundButton extends JButton {
-
-        private float opacity;
-
-        public RoundButton(String label) {
-            super(label);
-            Dimension size = getPreferredSize();
-            size.width = size.height = Math.max(size.width, size.height);
-            setPreferredSize(size);
-            setContentAreaFilled(false);
-            opacity = 1f;
-        }
-
-        public void setOpacity(float opacity) {
-            this.opacity = opacity;
-            repaint();
-        }
-
-        protected void paintComponent(Graphics g) {
-            if(getModel().isArmed()) {
-                g.setColor(Color.lightGray);
-            } else {
-                g.setColor(getBackground());
-            }
-            g.fillOval(0, 0, getSize().width-1, getSize().height-1);
-            super.paintComponent(g);
-        }
-
-        protected void paintBorder(Graphics g) {
-            g.setColor(getForeground());
-            g.drawOval(0, 0, getSize().width-1, getSize().height-1);
-        }
-
-        Shape shape;
-        public boolean contains(int x, int y) {
-            if(shape == null || !shape.getBounds().equals(getBounds())) {
-                shape = new Ellipse2D.Float(0, 0,
-                        getWidth(), getHeight());
-            }
-            return shape.contains(x, y);
-        }
-
-        @Override
-        public void paint(Graphics g) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
-            super.paint(g2);
-            g2.dispose();
         }
     }
 }
---
+++ src/presentation/RoundButton.java
@@ -0,0 +1,71 @@
+package presentation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class RoundButton extends JButton {
+
+    private float opacity;
+    private static float MAX_ALPHA = 0.8f;
+    private static float MIN_ALPHA = 0.2f;
+    private Color color;
+
+    public RoundButton(String text, Color color) {
+        super(text);
+        Dimension size = getPreferredSize();
+        size.width = size.height = Math.max(size.width, size.height);
+        setPreferredSize(size);
+        setContentAreaFilled(false);
+        setOpacity(MAX_ALPHA);
+        setBackground(color);
+        this.color = color;
+    }
+
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        if(enabled) {
+            setOpacity(MAX_ALPHA);
+            setBackground(color);
+        } else {
+            setOpacity(MIN_ALPHA);
+            setBackground(Color.GRAY);
+        }
+    }
+
+    private void setOpacity(float opacity) {
+        this.opacity = opacity;
+        repaint();
+    }
+
+    protected void paintComponent(Graphics g) {
+        if(getModel().isArmed()) {
+            g.setColor(Color.lightGray);
+        } else {
+            g.setColor(getBackground());
+        }
+        g.fillOval(0, 0, getSize().width-1, getSize().height-1);
+        super.paintComponent(g);
+    }
+
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground());
+        g.drawOval(0, 0, getSize().width-1, getSize().height-1);
+    }
+
+    Shape shape;
+    public boolean contains(int x, int y) {
+        if(shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
+        }
+        return shape.contains(x, y);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
+        super.paint(g2);
+        g2.dispose();
+    }
+}
