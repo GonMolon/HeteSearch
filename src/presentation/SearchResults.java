850e2a9a8010f5e020b15897d6299e4449630923
Gonzalo Solera <gonzalo1796@gmail.com>
2016-05-27 19:44:41 +0200

    GraphStream added

--- src/presentation/SearchResults.java
+++ src/presentation/SearchResults.java
@@ -1,13 +1,14 @@
 package presentation;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SearchResults extends JPanel {
 
-    private JPanel panel;
     private JList list;
 
     public SearchResults() {
-
+        list = new JList();
+        add(list, BorderLayout.CENTER);
     }
 }
