b96787d7513a700cc784148b5bd479a74301a998
Gonzalo Solera <gonzalo1796@gmail.com>
2016-05-28 16:34:42 +0200

    Removing Result object between presentation and domain2

--- src/presentation/utils/NodeTextField.java
+++ src/presentation/utils/NodeTextField.java
@@ -1,8 +1,6 @@
 package presentation.utils;
 
 
-import domain.GraphSearch;
-import domain.Node;
 import domain.NodeType;
 import presentation.PresentationController;
 
@@ -33,23 +31,20 @@ public class NodeTextField extends AutoClearTextField {
     }
 
     public void focusLost(FocusEvent e) {
-        ArrayList<GraphSearch.Result> results = presentationController.simpleSearch(nodeType, getText());
+        ArrayList<Integer> results = presentationController.simpleSearch(nodeType, getText());
         if(results.size() == 0 || getText().length() == 0) {
             id = -1;
             setText("");
             super.focusLost(e);
         } else if(results.size() == 1) {
-            id = results.get(0).from.getId();
-            setText(results.get(0).from.getValue());
+            id = results.get(0);
+            setText(presentationController.getNodeValue(nodeType, id));
         } else {
-            ArrayList<Integer> ids = new ArrayList<Integer>();
             ArrayList<String> names = new ArrayList<String>();
             for(int i = 0; i < results.size(); ++i) {
-                Node node = results.get(i).from;
-                ids.add(node.getId());
-                names.add(node.getValue());
+                names.add(presentationController.getNodeValue(nodeType, results.get(i)));
             }
-            InstanceSelectionDialog instanceSelectionDialog = new InstanceSelectionDialog(ids, names, "Pick an element", "You need to specify the node.\nPick the one you want:");
+            InstanceSelectionDialog instanceSelectionDialog = new InstanceSelectionDialog(results, names, "Pick an element", "You need to specify the node.\nPick the one you want:");
             id = instanceSelectionDialog.getIdChosen();
             setText(presentationController.getNodeValue(nodeType, id));
         }
@@ -57,8 +52,7 @@ public class NodeTextField extends AutoClearTextField {
 
     public void setNodeType(NodeType nodeType) {
         this.nodeType = nodeType;
-        ArrayList<GraphSearch.Result> result = presentationController.simpleSearch(nodeType, getText());
-        if(result.size() != 1) {
+        if(presentationController.simpleSearch(nodeType, getText()).size() != 1) {
             reset();
         }
     }
