aa707bee1c3577acae12993447be7d97ca25d2e8
David Álvarez <davidalro@gmail.com>
2016-05-31 17:02:35 +0200

    Modificado main para ejecución en consola (sin probar aun!)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
+import presentation.CommandLineInterface;
 import presentation.PresentationController;
 
 import javax.swing.*;
+import java.io.Console;
 
 public class Main {
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
-                        } catch (InstantiationException e) {
-                            e.printStackTrace();
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        } catch (UnsupportedLookAndFeelException e) {
-                            e.printStackTrace();
+        Console console = System.console();
+        if (console == null) {
+            javax.swing.SwingUtilities.invokeLater(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (InstantiationException e) {
+                                e.printStackTrace();
+                            } catch (IllegalAccessException e) {
+                                e.printStackTrace();
+                            } catch (UnsupportedLookAndFeelException e) {
+                                e.printStackTrace();
+                            }
+                            PresentationController presentationController = new PresentationController();
                         }
-                        PresentationController presentationController = new PresentationController();
                     }
-                }
-        );
+            );
+        } else {
+            CommandLineInterface cli = new CommandLineInterface();
+            cli.run();
+        }
     }
 }
