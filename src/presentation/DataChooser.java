54d793a940d7191b44d87e5b3a8438fcb9fc17d8
Gonzalo Solera <gonzalo1796@gmail.com>
2016-06-01 03:21:05 +0200

    some fixing

--- src/presentation/DataChooser.java
+++ src/presentation/DataChooser.java
@@ -9,9 +9,7 @@ import java.io.File;
 public class DataChooser extends JFileChooser{
 
     String path;
-    boolean correct_directory;
-    boolean found;
-    final String[] default_files = {"author.txt", "author_label.txt", "conf.txt", "conf_label.txt", "paper_label.txt", "paper.txt",
+    static String[] default_files = {"author.txt", "author_label.txt", "conf.txt", "conf_label.txt", "paper_label.txt", "paper.txt",
                                     "paper_author.txt", "paper_conf.txt", "paper_term.txt", "term.txt"};
 
     public DataChooser(Component c, boolean export) throws DataChooserException {
@@ -30,14 +28,12 @@ public class DataChooser extends JFileChooser{
         } else {
             throw new DataChooserException(DataChooserException.Error.NOT_SELECTED);
         }
-        // CONTROL DE ERRORES, QUE EN EL DIRECTORIO HAYA LOS ARCHIVOS POR DEFECTO (AUTHOR, PAPER, ...)
-        //LISTFILES desde FILE
-        correct_directory = true;
+        boolean correct_directory = true;
         String[] files = folder.list();
         int i = 0;
         if(!export)
             while(i < default_files.length && correct_directory){
-                found = false;
+                boolean found = false;
                 for (int j = 0; j < files.length && !found; ++j) {
                     if (files[j].equals(default_files[i])){
                         found = true;
